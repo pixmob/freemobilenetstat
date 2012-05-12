2ec745b277200878b74df3ef7be3b2e7269fd56d
Alexandre Roman <alexandre.roman@gmail.com>
2012-05-12 02:05:19 +0200

    Hide window title if the action bar is not available

--- src/org/pixmob/freemobile/netstat/ui/Netstat.java
+++ src/org/pixmob/freemobile/netstat/ui/Netstat.java
@@ -20,6 +20,7 @@ import org.pixmob.freemobile.netstat.MonitorService;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Window;
@@ -32,6 +33,10 @@ public class Netstat extends FragmentActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+        }
         
         if (getSupportFragmentManager().findFragmentById(android.R.id.content) == null) {
             final StatisticsFragment f = new StatisticsFragment();
