77060bb35536f05728becf41102c63541c63c868
Alexandre Roman <alexandre.roman@gmail.com>
2012-05-16 17:49:29 +0200

    Display license agreement in a dialog

--- src/org/pixmob/freemobile/netstat/ui/Preferences.java
+++ src/org/pixmob/freemobile/netstat/ui/Preferences.java
@@ -18,8 +18,16 @@ package org.pixmob.freemobile.netstat.ui;
 import static org.pixmob.freemobile.netstat.Constants.SP_NAME;
 import static org.pixmob.freemobile.netstat.Constants.TAG;
 
-import org.pixmob.freemobile.netstat.R;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+import org.pixmob.freemobile.netstat.R;
+import org.pixmob.freemobile.netstat.util.IOUtils;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
@@ -29,6 +37,9 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Application preferences screen.
@@ -71,7 +82,7 @@ public class Preferences extends PreferenceActivity implements
         if (SP_KEY_CHANGELOG.equals(k)) {
             openBrowser(getString(R.string.url_changelog));
         } else if (SP_KEY_LICENSE.equals(k)) {
-            openBrowser(getString(R.string.url_license));
+            showDialog(0);
         }
         
         return true;
@@ -82,5 +93,41 @@ public class Preferences extends PreferenceActivity implements
         i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY
                 | Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
         startActivity(i);
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        final StringBuilder licenseText = new StringBuilder(1024);
+        loadAsset("NOTICE.txt", licenseText);
+        licenseText.append('\n');
+        loadAsset("LICENSE.txt", licenseText);
+        
+        final View licenseContent = LayoutInflater.from(this).inflate(
+            R.layout.license, null);
+        final TextView license = (TextView) licenseContent
+                .findViewById(R.id.license);
+        license.setText(licenseText);
+        
+        return new AlertDialog.Builder(this).setTitle(R.string.pref_license)
+                .setView(licenseContent)
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setPositiveButton(android.R.string.ok, null).create();
+    }
+    
+    private void loadAsset(String fileName, StringBuilder buf) {
+        InputStream input = null;
+        try {
+            input = getResources().getAssets().open(fileName);
+            
+            final BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(input));
+            for (String line; (line = reader.readLine()) != null;) {
+                buf.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to load asset " + fileName, e);
+        } finally {
+            IOUtils.close(input);
+        }
     }
 }
