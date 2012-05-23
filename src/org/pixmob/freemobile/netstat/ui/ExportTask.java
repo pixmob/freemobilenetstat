6592b635140746fb9eae82203b12d7aaf229cb12
Alexandre Roman <alexandre.roman@gmail.com>
2012-05-24 01:06:14 +0200

    Fix data export (issue #6)

--- src/org/pixmob/freemobile/netstat/ui/ExportTask.java
+++ src/org/pixmob/freemobile/netstat/ui/ExportTask.java
@@ -22,6 +22,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 import org.pixmob.freemobile.netstat.R;
 import org.pixmob.freemobile.netstat.content.NetstatContract.Events;
@@ -33,7 +36,6 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
-import android.text.format.DateFormat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -45,7 +47,7 @@ class ExportTask extends AsyncTask<Void, Integer, Boolean> {
     private static final String DIALOG_TAG = "export";
     private static final String LINE_SEP = "\r\n";
     private static final String COL_SEP = ";";
-    private static final String DATE_FORMAT = "HH:mm:ss dd/MM/YYYY";
+    private static final String DATE_FORMAT = "dd/MM/yyyy HH:mm:ss";
     private final Context context;
     private FragmentManager fragmentManager;
     
@@ -131,6 +133,9 @@ class ExportTask extends AsyncTask<Void, Integer, Boolean> {
                 "freemobilenetstat.csv");
         Log.i(TAG, "Exporting database to " + outputFile.getPath());
         
+        final DateFormat dateFormatter = new SimpleDateFormat(DATE_FORMAT);
+        dateFormatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+        
         final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream(outputFile), "UTF-8"));
         
@@ -139,6 +144,7 @@ class ExportTask extends AsyncTask<Void, Integer, Boolean> {
             new String[] { Events.TIMESTAMP, Events.MOBILE_OPERATOR,
                     Events.MOBILE_CONNECTED, Events.WIFI_CONNECTED,
                     Events.BATTERY_LEVEL, Events.SCREEN_ON }, null, null, null);
+        
         try {
             final int rowCount = c.getCount();
             int currentRow = 0;
@@ -153,14 +159,14 @@ class ExportTask extends AsyncTask<Void, Integer, Boolean> {
             
             while (c.moveToNext()) {
                 final long t = c.getLong(0);
-                final String mobOp = c.getString(1);
-                final boolean mobConn = c.getInt(2) == 1;
-                final boolean wifiOn = c.getInt(3) == 1;
+                final String mobOp = c.isNull(1) ? "" : c.getString(1);
+                final int mobConn = c.getInt(2) == 1 ? 1 : 0;
+                final int wifiOn = c.getInt(3) == 1 ? 1 : 0;
                 final int bat = c.getInt(4);
-                final boolean screenOn = c.getInt(5) == 1;
+                final int screenOn = c.getInt(5) == 1 ? 1 : 0;
                 
                 buf.delete(0, buf.length());
-                buf.append(DateFormat.format(DATE_FORMAT, t)).append(COL_SEP)
+                buf.append(dateFormatter.format(t)).append(COL_SEP)
                         .append(mobOp).append(COL_SEP).append(mobConn)
                         .append(COL_SEP).append(wifiOn).append(COL_SEP)
                         .append(screenOn).append(COL_SEP).append(bat)
