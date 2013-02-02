916bce9a889cdd72b3a6e68acc69ad77b01323bf
Alexandre Roman <alexandre.roman@gmail.com>
2013-02-02 18:09:07 +0100

    Remove object allocation while painting the view

--- src/org/pixmob/freemobile/netstat/ui/MobileNetworkChart.java
+++ src/org/pixmob/freemobile/netstat/ui/MobileNetworkChart.java
@@ -28,9 +28,11 @@ import android.view.View;
 
 /**
  * Custom component showing mobile network use with a pie chart.
+ * 
  * @author Pixmob
  */
 public class MobileNetworkChart extends View {
+    private final RectF circleBounds = new RectF();
     private float startAngle = 270;
     private float orangeAngle;
     private float freeMobileAngle;
@@ -40,52 +42,45 @@ public class MobileNetworkChart extends View {
     private Paint freeMobilePaint;
     private Paint unknownPaint;
     private int circleColor = -1;
-    
+
     public MobileNetworkChart(final Context context, final AttributeSet attrs) {
         super(context, attrs);
         setData(75, 10);
     }
-    
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        
+
         // Lazy initialize paint properties, once.
         if (orangePaint == null) {
             orangePaint = new Paint();
             orangePaint.setAntiAlias(true);
             orangePaint.setStyle(Paint.Style.FILL);
-            
-            final int c1 = getResources().getColor(
-                R.color.orange_network_color1);
-            final int c2 = getResources().getColor(
-                R.color.orange_network_color2);
-            orangePaint.setShader(new LinearGradient(0, 0, 0, getHeight(), c1,
-                    c2, Shader.TileMode.CLAMP));
+
+            final int c1 = getResources().getColor(R.color.orange_network_color1);
+            final int c2 = getResources().getColor(R.color.orange_network_color2);
+            orangePaint.setShader(new LinearGradient(0, 0, 0, getHeight(), c1, c2, Shader.TileMode.CLAMP));
         }
         if (freeMobilePaint == null) {
             freeMobilePaint = new Paint();
             freeMobilePaint.setAntiAlias(true);
             freeMobilePaint.setStyle(Paint.Style.FILL);
-            
-            final int c1 = getResources().getColor(
-                R.color.free_mobile_network_color1);
-            final int c2 = getResources().getColor(
-                R.color.free_mobile_network_color2);
-            freeMobilePaint.setShader(new LinearGradient(0, 0, 0, getHeight(),
-                    c1, c2, Shader.TileMode.CLAMP));
+
+            final int c1 = getResources().getColor(R.color.free_mobile_network_color1);
+            final int c2 = getResources().getColor(R.color.free_mobile_network_color2);
+            freeMobilePaint.setShader(new LinearGradient(0, 0, 0, getHeight(), c1, c2, Shader.TileMode.CLAMP));
         }
         if (unknownPaint == null) {
             unknownPaint = new Paint();
             unknownPaint.setAntiAlias(true);
             unknownPaint.setStyle(Paint.Style.FILL);
-            unknownPaint.setColor(getResources().getColor(
-                R.color.unknown_mobile_network_color));
+            unknownPaint.setColor(getResources().getColor(R.color.unknown_mobile_network_color));
         }
         if (circleColor == -1) {
             circleColor = getResources().getColor(R.color.pie_border_color);
         }
-        
+
         if (arcBorderPaint == null) {
             arcBorderPaint = new Paint();
             arcBorderPaint.setAntiAlias(true);
@@ -98,53 +93,45 @@ public class MobileNetworkChart extends View {
             arcFillPaint.setAntiAlias(true);
             arcFillPaint.setStyle(Paint.Style.FILL);
         }
-        
+
         final int w = getWidth();
         final int h = getHeight();
         final int radius = Math.min(w, h) - 4;
         final int startX = (w - radius) / 2;
         final int startY = (h - radius) / 2;
-        final RectF circleBounds = new RectF(startX, startY, startX + radius,
-                startY + radius);
-        
+        circleBounds.set(startX, startY, startX + radius, startY + radius);
+
         if (orangeAngle > 0) {
-            canvas.drawArc(circleBounds, startAngle, orangeAngle, true,
-                arcBorderPaint);
-            canvas.drawArc(circleBounds, startAngle, orangeAngle, true,
-                orangePaint);
+            canvas.drawArc(circleBounds, startAngle, orangeAngle, true, arcBorderPaint);
+            canvas.drawArc(circleBounds, startAngle, orangeAngle, true, orangePaint);
         }
         if (freeMobileAngle > 0) {
-            canvas.drawArc(circleBounds, startAngle + orangeAngle,
-                freeMobileAngle, true, arcBorderPaint);
-            canvas.drawArc(circleBounds, startAngle + orangeAngle,
-                freeMobileAngle, true, freeMobilePaint);
+            canvas.drawArc(circleBounds, startAngle + orangeAngle, freeMobileAngle, true, arcBorderPaint);
+            canvas.drawArc(circleBounds, startAngle + orangeAngle, freeMobileAngle, true, freeMobilePaint);
         }
-        
+
         final float unknownAngle = 360 - orangeAngle - freeMobileAngle;
         if (unknownAngle > 0) {
-            canvas.drawArc(circleBounds, startAngle + orangeAngle
-                    + freeMobileAngle, unknownAngle, true, arcBorderPaint);
-            canvas.drawArc(circleBounds, startAngle + orangeAngle
-                    + freeMobileAngle, unknownAngle, true, unknownPaint);
+            canvas.drawArc(circleBounds, startAngle + orangeAngle + freeMobileAngle, unknownAngle, true, arcBorderPaint);
+            canvas.drawArc(circleBounds, startAngle + orangeAngle + freeMobileAngle, unknownAngle, true, unknownPaint);
         }
     }
-    
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec),
-            MeasureSpec.getSize(heightMeasureSpec));
+        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), MeasureSpec.getSize(heightMeasureSpec));
     }
-    
+
     public void setData(int percentOnOrange, int percentOnFreeMobile) {
         orangeAngle = percentToAngle(normalizePercent(percentOnOrange));
         freeMobileAngle = percentToAngle(normalizePercent(percentOnFreeMobile));
         startAngle = freeMobileAngle / 2f;
     }
-    
+
     private static float percentToAngle(int p) {
         return p * 360 / 100f;
     }
-    
+
     private static int normalizePercent(int p) {
         if (p < 0) {
             return 0;
