9053ac0781866b17ad4991854f52dc2efdda8ed9
gilbsgilbs <gilbsgilbert@gmail.com>
2014-12-15 16:30:51 +0100

    Removed things to remove.

--- freemobilenetstat/src/main/java/org/pixmob/freemobile/netstat/ui/Netstat.java
+++ freemobilenetstat/src/main/java/org/pixmob/freemobile/netstat/ui/Netstat.java
@@ -80,8 +80,6 @@ public class Netstat extends FragmentActivity {
             // Store the current application version.
             final SharedPreferences.Editor prefsEditor = prefs.edit();
             prefsEditor.putInt(versionKey, applicationVersion);
-            prefsEditor.putString(SP_KEY_STAT_NOTIF_SOUND_FREE_MOBILE, null); // FIXME > We should delete this in next releases.
-            prefsEditor.putString(SP_KEY_STAT_NOTIF_SOUND_ORANGE, null); // FIXME > We should delete this in next releases.
             Features.getFeature(SharedPreferencesSaverFeature.class).save(prefsEditor);
 
             // The application was updated: let's show changelog.
