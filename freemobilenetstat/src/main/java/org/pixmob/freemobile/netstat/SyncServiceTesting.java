711276374ba1c578d1636d7d04c2378947bc43b3
Nils Le Roux <nilsleroux@gmail.com>
2015-06-21 17:00:37 +0200

    Release version 2.2.0.

--- freemobilenetstat/src/main/java/org/pixmob/freemobile/netstat/SyncServiceTesting.java
+++ freemobilenetstat/src/main/java/org/pixmob/freemobile/netstat/SyncServiceTesting.java
@@ -65,7 +65,7 @@ import static org.pixmob.freemobile.netstat.Constants.TAG;
  */
 public class SyncServiceTesting extends IntentService {
     private static final Random RANDOM = new Random();
-    private static final String SERVER_API_URL = "http://freemobile-netstat-gae.appspot.com/";
+    private static final String SERVER_API_URL = "http://freemobile-netstat.appspot.com/";
     private static final int SERVER_API_VERSION = 1;
     private static final String EXTRA_DEVICE_REG = "org.pixmob.freemobile.netstat.deviceReg";
     private static final long DAY_IN_MILLISECONDS = 86400 * 1000;
@@ -514,10 +514,10 @@ public class SyncServiceTesting extends IntentService {
     }
 
     private static class UploadDatabaseHelper extends SQLiteOpenHelper {
-        private static final int UPLOAD_DATABASE_VERSION = 2;
+        private static final int UPLOAD_DATABASE_VERSION = 3;
 
         public UploadDatabaseHelper(final Context context) {
-            super(context, "upload.db", null, UPLOAD_DATABASE_VERSION);
+            super(context, "upload_testing.db", null, UPLOAD_DATABASE_VERSION);
         }
 
         @Override
